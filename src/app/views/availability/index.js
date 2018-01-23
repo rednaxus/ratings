5bd314921942b68ad67de33779f7fdbf05c34569
Alan Kaiser <rednaxus@gmail.com>
2018-01-22 23:46:51 -0800

    some tweaks, animated views

--- src/app/views/availability/index.js
+++ src/app/views/availability/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Carousel from 'nuka-carousel'
+import { AnimatedView } from '../../components'
 
 class Availability extends Component {
   constructor(props, { authData }) {
@@ -15,41 +16,43 @@ class Availability extends Component {
 
   render() {
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Availability for rounds</h1>
-            <p><strong>Schedule your availability <span style={{color:"green"}}>{/*this.props.authData.name*/}</span> for coming ratings rounds!</strong> </p>
-            <div className="availability-carousel">
-              <Carousel 
-                slidesToShow={3} 
-                cellSpacing={1}
-                initialSlideHeight={150}
-                initialSlidewidth={200} 
-                framePadding="20px"
-                cellAlign="left" 
-                ref="carousel"
-                slideIndex={this.state.slideIndex}
-                afterSlide={newSlideIndex => this.setState({ slideIndex: newSlideIndex })}
-              >
-                {Array(8).fill().map((_, i) => (
-                  <div className="availability-container" key={i} >
-                    <div>Week of Jan 1{i*7}</div>
-                    <div>Survey due Jan 1{(i+1)*7}</div>
-                    <br/>
-                    <div>Serve as:</div>
-                    <div className="btn-group" style={{textAlign:"center"}} role="group" aria-label="..." >
-                      <button type="button" className="btn btn-default">Lead</button>
-                      <button type="button" className="btn btn-default">Jurist</button>
+      <AnimatedView>
+        <main className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              <h1>Availability for rounds</h1>
+              <p><strong>Schedule your availability <span style={{color:"green"}}>{/*this.props.authData.name*/}</span> for coming ratings rounds!</strong> </p>
+              <div className="availability-carousel">
+                <Carousel 
+                  slidesToShow={3} 
+                  cellSpacing={1}
+                  initialSlideHeight={150}
+                  initialSlidewidth={200} 
+                  framePadding="20px"
+                  cellAlign="left" 
+                  ref="carousel"
+                  slideIndex={this.state.slideIndex}
+                  afterSlide={newSlideIndex => this.setState({ slideIndex: newSlideIndex })}
+                >
+                  {Array(8).fill().map((_, i) => (
+                    <div className="availability-container" key={i} >
+                      <div>Week of Jan 1{i*7}</div>
+                      <div>Survey due Jan 1{(i+1)*7}</div>
+                      <br/>
+                      <div>Serve as:</div>
+                      <div className="btn-group" style={{textAlign:"center"}} role="group" aria-label="..." >
+                        <button type="button" className="btn btn-default">Lead</button>
+                        <button type="button" className="btn btn-default">Jurist</button>
+                      </div>
                     </div>
-                  </div>
-                ))}
-              </Carousel>
+                  ))}
+                </Carousel>
+              </div>
+              <h3>Tokens Covered</h3>
             </div>
-            <h3>Tokens Covered</h3>
           </div>
-        </div>
-      </main>
+        </main>
+      </AnimatedView>
     )
   }
 }
--- src/app/views/rules/index.js
+++ src/app/views/rules/index.js
@@ -1,27 +1,28 @@
 import React, { PureComponent } from 'react'
-
+import { AnimatedView } from '../../components'
 
 export class Rules extends PureComponent {
 
   render() {
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Veva Ratings Manager Rules</h1>
-            <p><strong>Congratulations!</strong> Welcome to the <span className="dog">Veva Ratings Agency</span> smart contract.</p>
+      <AnimatedView>
+        <main className="container">
+          <div className="pure-g">
+            <div className="pure-u-1-1">
+              <h1>Veva Ratings Manager Rules</h1>
+              <p><strong>Congratulations!</strong> Welcome to the <span className="dog">Veva Ratings Agency</span> smart contract.</p>
+            </div>
           </div>
-        </div>
-        <div className="panel panel-default">
-          <div className="panel-heading">
-          Current Shares Listed
+          <div className="panel panel-default">
+            <div className="panel-heading">
+            Current Shares Listed
+            </div>
+            <div className="panel-body">
+              <div>The rules is:</div>
+            </div>
           </div>
-          <div className="panel-body">
-            <div>The rules is:</div>
-          </div>
-        </div>
-
-      </main>
+        </main>
+      </AnimatedView>
     )
   }
 }

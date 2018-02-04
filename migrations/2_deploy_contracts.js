6e6ba96dd363c09bf2b67ef3a5fbdea8c2231a68
Alan Kaiser <rednaxus@gmail.com>
2018-02-04 13:13:57 -0800

    add some parameters to deploy code so new version of truffle won't barf

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,8 +3,8 @@ var AnalystRegistry = artifacts.require("AnalystRegistry")
 var RatingAgency = artifacts.require("RatingAgency")
 
 module.exports = function(deployer) {
-  deployer.deploy(TokenERC20)
+  deployer.deploy(TokenERC20,10000,"dummy_name","dummy_token")
   deployer.deploy(AnalystRegistry)
-  deployer.deploy(RatingAgency)
+  deployer.deploy(RatingAgency,0)
 }
 

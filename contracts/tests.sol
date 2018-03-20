c454cc00bc2d2ba57e0388c068a54f34809d859a
Alan Kaiser <rednaxus@gmail.com>
2018-03-20 15:45:33 -0700

    token information rendering bootstrap with top 16 by capitalization

--- contracts/tests.sol
+++ contracts/tests.sol
@@ -16,7 +16,7 @@ import "./TokenERC20.sol";
 import "./RatingAgency.sol";
 
 contract test2 {
-    address defaultRa = 0x0af2ee5c1c1a2e9109d246e04374271e601bbc5d;
+    address defaultRa = 0x594274b7619fd1b9ce9b83b771bcf6ec4a4efa95;
     using strings for *;
     RatingAgency ratingAgency;
     mapping( uint => address ) tokens;
@@ -70,6 +70,30 @@ contract test2 {
         }
     }
     
+    address[16] live_tokens = [
+        0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0, //EOS 
+        0xf230b790e05390fc8295f4d3f60332c93bed42e2, // Tronix
+        0xd850942ef8811f2a866692a623011bde52a462c1, // VeChain
+        0xd26114cd6ee289accf82350c8d8487fedb8a0c07, // OMG
+        0xb5a5f22694352c15b00323844ad545abb2b11028, // Icon
+        0xb8c77482e45f1f44de1745f52c74426c631bdd52, // BnB
+        0xe0b7927c4af23765cb51314a0e0521a9645f0e2a, // Digix
+        0xd4fa1460f537bb9085d22c7bccb5dd450ef28e3a, // Populous
+        0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2, // Maker
+        0x744d70fdbe2ba4cf95131626614a1763df805b9e, // status
+        0x168296bb09e24a88805cb9c33356536b980d3fc5, // RHOC
+        0xe94327d07fc17907b4db788e5adf2ed424addff6,  // Reputation
+        0x5ca9a71b1d01849c0a95490cc00559717fcf0d1d, // Aeternity
+        0xcb97e65f07da24d46bcdd078ebebd7c6e6e3d750, // Byteom
+        0xb7cb1c96db6b22b0d3d9536e0108d062bd488f74, // Walton
+        0x4ceda7906a5ed2179785cd3a40a69ee8bc99c466 // Aeon 
+    ];
+
+    function bootstrapTokens( ) public { 
+        for ( uint i = 0; i < live_tokens.length; i++ ) {
+            ratingAgency.coverToken( live_tokens[i], 0 );
+        }
+    }
     event RoundInfo(
         uint16 cycle,
         uint time, 
--- src/app/views/token/Token.js
+++ src/app/views/token/Token.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Moment from 'react-moment'
 import * as _ from 'lodash'
 
-import { AnimatedView } from '../../components'
+import { AnimatedView, Panel } from '../../components'
 
 class Token extends Component {
 
@@ -33,11 +35,43 @@ class Token extends Component {
     let token = idx === -1 ? { }: tokens.data[idx] 
     console.log('props',this.props)
     console.log('token',tokens,idx,token)
+    let title = "Token: "+ token.name + " => " + token.symbol
+    const tokenLink = (address) => "https://etherscan.io/address/"+token.address
     return(
       <AnimatedView>
         <div className="simpleContainer">
-          <h1>Token {token.name}</h1>
-          <div>{token.address}</div>
+          <Panel title={title} hasTitle={true} bodyBackGndColor={'#F4F5F6'}>
+            <div className="row">
+              <div className="col-md-4">
+                { token.price &&   
+                  <div>
+                    <div>rate: <span className="price-data">{token.price.rate}</span></div>
+                    <div>diff: <span className="price-data">{token.price.diff}</span></div>
+                    <div>ts: <span className="price-data">{token.price.ts}</span></div>
+                    <div>volume(24): <span className="price-data">{token.price.volume24h}</span></div>
+                    <div>currency: <span className="price-data">{token.price.currency}</span></div>
+                    <div>available supply: <span className="price-data">{token.price.availableSupply}</span></div>
+                    <div>market cap: <span className="price-data">{token.price.marketCapUsd}</span></div>
+                  </div>
+                }
+                </div>
+              <div className = "col-md-4">      
+                <div>Updated: <Moment className="token-data" format="YYYY-MM-DD HH:mm" date={new Date(token.lastUpdated*1000)} /></div>
+                <div>Supply: <span className="token-data">{token.totalSupply}</span></div>
+                <div>Transfers: <span className="token-data">{token.transfersCount}</span></div>
+                <div>Holders: <span className="token-data">{token.holdersCount}</span></div>
+                <div>Ops: <span className="token-data">{token.countOps}</span></div>
+              </div>
+              <div className = "col-md-4">
+                <div><a className="small" href={tokenLink(token.address)}>{token.address}</a></div>
+                <div><a href={token.description}>description</a></div>
+              </div>
+            </div>
+          </Panel>
+          <Panel title="Token Rounds History" hasTitle={true} bodyBackGndColor={'#F4F5F6'}>
+            <div>Current status: </div>
+
+          </Panel>
         </div>
       </AnimatedView>
     )

161323a46cc814fc21cb28c12e8dcfaa27af5b7e
ry <rjyoungblom@gmail.com>
2018-03-26 22:39:38 -0700

    card mockups

--- src/app/components/analysts/AnalystMessages.js
+++ src/app/components/analysts/AnalystMessages.js
@@ -1,4 +1,4 @@
-/* 
+/*
     AnalystMessages takes the current state and generates a list display of messages based on the state
 */
 
@@ -12,6 +12,47 @@ import PropTypes from 'prop-types'
 import { Grid } from 'react-redux-grid'
 import { Notification } from '../../components'
 import events from './events'
+import { Glyphicon }                     from 'react-bootstrap'
+
+
+
+var glyphStyle = {
+  fontSize: "100px"
+};
+
+var spanStyle = {
+  display: "flex",
+  alignItems: "center",
+  paddingRight: "30px"
+};
+
+var cardStyle = {
+  maxWidth: "50%"
+};
+
+var cardFlex = {
+  display:"flex",
+  flexDirection: "row",
+  justifyContent: 'space-around'
+};
+
+var infoRow = {
+  width: "100%",
+  display:"flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  justifyContent: "center"
+};
+
+var buttonFlex = {
+  color: "blue",
+  maxWidth: "50%",
+display: "flex",
+flexDirection: "row",
+justifyContent: "center",
+alignItems: "flex-end"
+};
+
 
 const base_message = {
   url: '',
@@ -21,10 +62,10 @@ const base_message = {
   type: 'success'
 }
 const message_types = {
-  new_round_scheduling: { 
+  new_round_scheduling: {
     ...base_message,
     url: '/analyst/availability',
-    primary: () => { 
+    primary: () => {
       return <div>Upcoming rounds available<span>set your availability</span></div>
     },
     secondary: () => {
@@ -42,10 +83,10 @@ const message_types = {
 
 
 const data = [
-    {text: '3 upcoming rounds available, set your availability', type: 'roundsUpcoming'},
-    {text: 'Currently active on round 3, token XXOH, first survey due by ....', type: 'type2' },
-    {text: 'Round scheduled, awaiting confirmation', type: 'round_confirmation' },
-    {text: 'You have a brief due by xxx for round 4, upload your brief', type: 'brief_upload'}
+    {text: '4 new rounds available!', type: 'roundsUpcoming', glyph: <Glyphicon glyph="calendar"></Glyphicon>, body: "Please set your availability via your profile", due: "1h:12m"},
+    {text: 'Currently active: Round 3!', type: 'type2', glyph: <Glyphicon glyph="star-empty"></Glyphicon>, body: "token XXOH, first survey due by ....", due: "1d:3h:49m"},
+    {text: 'Round scheduled!', type: 'round_confirmation', glyph: <Glyphicon glyph="check"></Glyphicon>, body: "Status: awaiting confirmation", due: "0h:31m"},
+    {text: 'Brief Due', type: 'brief_upload', glyph: <Glyphicon glyph="paperclip"></Glyphicon>, body: "You have a brief due by xxx for round 4, please upload", due: "4h:55m"}
 ]
 
 export const AnalystMessages = ({ store }) => {
@@ -55,30 +96,44 @@ export const AnalystMessages = ({ store }) => {
 
   return messages().map( message =>
     <div className = "row">
-      <div className="card">
-        <img className="card-img-top" src="https://cdn.history.com/sites/2/2014/02/redscare-H.jpeg"/>
-        <div className="card-block">
-          <figure className="profile">
-            <img src="http://success-at-work.com/wp-content/uploads/2015/04/free-stock-photos.gif" className="profile-avatar" alt=""/>
-          </figure>
-          <h4 className="card-title mt-3">{message.text}</h4>
-          <div className="meta">
-            <a>Friends</a>
+        <div className="panel panel-danger card" style={cardStyle}>
+
+          <div className="panel-heading">
+            <h3 className="panel-title">Due In: {message.due} </h3>
           </div>
-          <div className="card-text">
-            Blah blah with some blah blah.
-          </div>
+
+
+        <div className="panel-body" style={cardFlex}>
+            <div id="glyphSpan">
+              <span style={spanStyle}>
+              <span className="card-title mt-3" style={glyphStyle}>{message.glyph} </span>
+              </span>
+            </div>
+
+            <div style={infoRow}>
+              <span className="card-title mt-3">{message.text}</span>
+              <div className="meta">
+              <a> additional information: </a>
+              </div>
+              <div className="card-text">
+              {message.body}
+              <br />
+
+              </div>
+            </div>
+
+            <div style={buttonFlex}>
+            <button className="btn btn-secondary float-left">Go! </button> <button className="btn btn-secondary float-right btn-sm">later</button>
+            </div>
+
         </div>
-        <div className="card-footer">
-          <small>Last updated 3 mins ago</small>
-          <button className="btn btn-secondary float-right btn-sm">show</button>
-        </div>
+
       </div>
     </div>
   )
 
 
-} 
+}
 
 AnalystMessages.propTypes = {
     store: PropTypes.object.isRequired
@@ -86,4 +141,4 @@ AnalystMessages.propTypes = {
 
 AnalystMessages.defaultProps = {}
 
-export default AnalystMessages 
+export default AnalystMessages

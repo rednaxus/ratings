80da26d0d9b68726588443dbf7420fcdbb72c4f4
Alan Kaiser <rednaxus@gmail.com>
2018-04-17 21:59:46 -0700

    playing around with survey rating aesthetics put card width to 100%

--- src/app/components/jurist/Question.js
+++ src/app/components/jurist/Question.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import DropdownList from 'react-widgets/lib/DropdownList'
-import SelectList from 'react-widgets/lib/SelectList'
-import Multiselect from 'react-widgets/lib/Multiselect'
-import DateTimePicker from 'react-widgets/lib/DateTimePicker'
+import { SelectList, Multiselect, DateTimePicker } from 'react-widgets'
+//import Multiselect from 'react-widgets/lib/Multiselect'
+//import DateTimePicker from 'react-widgets/lib/DateTimePicker'
+import Rating from 'react-rating'
 import moment from 'moment'
 //import momentLocaliser from 'react-widgets/lib/localizers/moment'
 import { Panel } from 'react-bootstrap'
 import 'react-widgets/dist/css/react-widgets.css'
-import validate from './validate';
+import validate from './validate'
 
 //momentLocaliser(moment)
 
@@ -43,6 +44,94 @@ const renderSelectList = ({ input, data }) => (
   <SelectList {...input} onBlur={() => input.onBlur()} data={data} />
 )
 
+const renderRating = ({ input, data }) => (
+  <Rating   
+    emptySymbol="fa fa-star-o fa-2x fat"
+    fullSymbol="fa fa-star fa-2x fat"
+    {...input} 
+    initialRate={parseInt(input.value)} 
+    data={data} 
+  />
+)
+
+/*
+const renderSelectListButtons
+  <ButtonGroup>
+    <Button>1</Button>
+    <Button>2</Button>
+    <Button>3</Button>
+    <Button>4</Button>
+  </ButtonGroup>
+*/
+const btnStyles = {
+  border: '3px solid #f6e9db',
+  background: 'transparent',
+  borderRadius: '45px',
+  width: '250px'
+}
+
+const btnSelectedStyles = {
+  border: '3px solid #27313c',
+  background: 'transparent',
+  borderRadius: '45px',
+  width: '250px'
+}
+
+const MultiSelectBtnGroup = ({ input, data }) => {
+  let options = data
+  const values = input.value || [];
+
+  const handleClick = (value, select) => {
+    var index = values.indexOf(value);
+
+    if (select) {
+      if (index === -1) {
+          input.onChange([...values, value]);
+      }
+    } else {
+      if (index !== -1) {
+          input.onChange(values.filter(v => v !== value));
+      }
+    }
+  }
+
+  return (
+    <div className="row">
+      {options.map(o => {
+        const selected = values.indexOf(o.value) !== -1;
+        return (
+          <div className="col">
+            <div className="row m-auto rel">
+              <button
+                  type="button"
+                  className="mb-1 p-3 ml-auto mr-auto"
+                  style={selected ? btnSelectedStyles : btnStyles}
+                  onClick={e => handleClick(o.value, !selected)}
+              >
+                {o.content}
+                <span
+                    style={{ position: 'absolute', right: '15px', top: 0, bottom: 0, margin: 'auto' }}
+                    className="mb-1 d-flex justify-content-center align-items-center"
+                >
+                    {'zz'/*icons.plusIcon(selected)*/}
+                </span>
+              </button>
+
+              <input
+                type="hidden"
+                name={name}
+                value={o.value}
+                checked={selected}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  )
+};
+
+/*
 const renderDateTimePicker = ({ input: { onChange, value }, showTime }) => (
   <DateTimePicker
     onChange={onChange}
@@ -51,7 +140,7 @@ const renderDateTimePicker = ({ input: { onChange, value }, showTime }) => (
     value={!value ? null : new Date(value)}
   />
 )
-
+*/
 
 const Question = props => {
   const { handleSubmit, pristine, nextPage, previousPage, submitting, questionNumber, questionData } = props;
@@ -67,7 +156,7 @@ const Question = props => {
               <div className="question-body"><i className="fa fa-bar-chart fa-lg"/>{questionData.title}</div>
               <Field
                 name={"rating-"+questionNumber}
-                component={renderSelectList}
+                component={renderRating}
                 data={['1', '2', '3', '4', '5']}
               />
             </div>

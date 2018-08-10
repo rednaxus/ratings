ca7f5b94057f0db641197d56f4e62bd0c3dfbc22
Alan <alan@on40.org>
2018-08-10 19:33:39 +0000

    work in progress tele-bot

--- servers/tele-bot.js
+++ servers/tele-bot.js
@@ -13,7 +13,16 @@ const ikmStr = `${botStr}/InlineKeyboardMarkup`
 
 const stinkie_img = 'https://stinkie.one/static/media/logo.591c1b5d.jpg'
 
-const ratingPanel = [ { text:'1', url: smelly }, { text: '2', url: smelly } ]
+const ratingPanel = {"keyboard": [["Done","Done 2"], ["Update"], ["Log Time"]],"one_time_keyboard": true} //[ [ { text:'1', url: smelly }, { text: '2', url: smelly } ] ]
+
+//keyboard = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='18-02-28-141715.mp4', callback_data='2018-02-28 14:17:27')],[InlineKeyboardButton(text='18-02-28-135007.mp4', callback_data='2018-02-28 13:50:24')],])
+const rp2 =  { 
+    inline_keyboard: [
+      [{ text: 'Some button text 1', callback_data: '1' }],
+      [{ text: 'Some button text 2', callback_data: '2' }],
+      [{ text: 'Some button text 3', callback_data: '3' }]
+    ]
+}
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(
@@ -21,6 +30,8 @@ app.use(
     extended: true
   })
 ) // for parsing application/x-www-form-urlencoded
+
+let last_chat_id = 100
 
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
@@ -35,15 +46,17 @@ app.post('/new-message', function(req, res) {
   }
   const msg = m => m.toLowerCase()
 
-  const { message } = req.body
+  const { message, callback_query } = req.body
 
-  console.log('got another new message',message)
+  console.log('got another new message',req.body)
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
 
   if (message && message.text){ // text request messages
     const text = msg( message.text )
+    last_chat_id = message.chat.id
+
     if ( text.indexOf('stinkie') >= 0 ) {
 
       axios.post(imgStr,{ chat_id: message.chat.id, photo: stinkie_img }).then( success ).catch( error ) 
@@ -54,12 +67,16 @@ app.post('/new-message', function(req, res) {
 
     } else if ( text.indexOf('rate') >= 0 ) { //keyboard for ratings
 
-      axios.post( ikmStr, { inline_keyboard: ratingPanel }).then( success ).catch( error ) 
+      axios.post( msgStr, { chat_id: message.chat.id, text: 'test keyboard', reply_markup: rp2 }).then( success ).catch( error ) 
 
     }
     else return res.end()
+  } else if (callback_query) {
+      axios.post( msgStr, { chat_id: callback_query.from.id, text: callback_query.data } ).then( success ).catch( error )
   }
+  else return res.end()
 })
+
 
 // Finally, start our server
 app.listen(4000, function() {
@@ -67,3 +84,23 @@ app.listen(4000, function() {
 })
 
 
+/*
+  callback_query:
+   { id: '1583783509977761152',
+     from:
+      { id: 368753334,
+        is_bot: false,
+        first_name: 'alan',
+        last_name: 'k',
+        username: 'alan_on40',
+        language_code: 'en-US' },
+     message:
+      { message_id: 106,
+        from: [Object],
+        chat: [Object],
+        date: 1533925873,
+        text: 'test keyboard' },
+     chat_instance: '5779136896984275476',
+     data: '2' } }
+
+*/

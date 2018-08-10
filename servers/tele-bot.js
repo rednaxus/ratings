a970fb688711a3435218dac3d009cf815013a39b
Alan <alan@on40.org>
2018-08-10 01:03:10 +0000

    botwork

--- servers/tele-bot.js
+++ servers/tele-bot.js
@@ -3,6 +3,17 @@ var app = express()
 var bodyParser = require('body-parser')
 const axios = require('axios')
 
+const botStr = 'https://api.telegram.org/bot637622678:AAHX54UuHozJkwrIYm5vXAptuxZUWLpIuZ8'
+const smelly = 'https://agario-skins.org/images/skins/custom/smelly.png'
+
+const msgStr = `${botStr}/sendMessage`
+const imgStr = `${botStr}/sendPhoto`
+const ikmStr = `${botStr}/InlineKeyboardMarkup`
+
+const stinkie_img = 'https://stinkie.one/static/media/logo.591c1b5d.jpg'
+
+const ratingPanel = [ { text:'1', url: smelly }, { text: '2', url: smelly } ]
+
 app.use(bodyParser.json()) // for parsing application/json
 app.use(
   bodyParser.urlencoded({
@@ -12,38 +23,41 @@ app.use(
 
 //This is the route the API will call
 app.post('/new-message', function(req, res) {
+  const error = err => {
+    // ...and here if it was not
+    console.log('Error :', err)
+    res.end('Error :' + err)
+  }
+  const success = response => {
+    console.log('Message posted')
+    res.end('ok')
+  }
+  const msg = m => m.toLowerCase()
+
   const { message } = req.body
 
-  console.log(`got new message`,message)
+  console.log('got another new message',message)
 
   //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
 
-  if (!message || message.text.toLowerCase().indexOf('run round') < 0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
-  }
 
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios
-    .post(
-      'https://api.telegram.org/bot637622678:AAHX54UuHozJkwrIYm5vXAptuxZUWLpIuZ8/sendMessage',
-      {
-        chat_id: message.chat.id,
-        text: 'Ready to run for AOS token'
-      }
-    )
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
+  if (message && message.text){ // text request messages
+    const text = msg( message.text )
+    if ( text.indexOf('stinkie') >= 0 ) {
+
+      axios.post(imgStr,{ chat_id: message.chat.id, photo: stinkie_img }).then( success ).catch( error ) 
+
+    } else if ( text.indexOf('run round') >= 0 ) {
+
+      axios.post( msgStr,{ chat_id: message.chat.id, text: 'Ready to run for AOS token'} ).then( success ).catch( error ) 
+
+    } else if ( text.indexOf('rate') >= 0 ) { //keyboard for ratings
+
+      axios.post( ikmStr, { inline_keyboard: ratingPanel }).then( success ).catch( error ) 
+
+    }
+    else return res.end()
+  }
 })
 
 // Finally, start our server
